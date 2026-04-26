d8335aad4fe6f660349e13df3ef750b9ae8624ab
kontai <chokunzzz741852963@gmail.com>
2026-04-27 06:29:04 +0700

    auth.js
    auth.js

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,13 +1,10 @@
 const express = require('express')
-const router = express.Router()
+const router  = express.Router()
 const { register, login, getMe } = require('../Controllers/auth')
 const auth = require('../Middleware/auth')
 
-// ไม่ต้อง login
-router.post('/register', register)     // สมัครสมาชิก
-router.post('/login', login)           // เข้าสู่ระบบ
-
-// ต้อง login (มี auth middleware คั่น)
-router.get('/me', auth, getMe)         // ดูข้อมูลตัวเอง
+router.post('/register', register)
+router.post('/login',    login)
+router.get('/me', auth,  getMe)
 
 module.exports = router
