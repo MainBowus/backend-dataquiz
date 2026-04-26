a4049e3c5d90b232f3cbc182ce54a02c45f0a3e4
kontai <chokunzzz741852963@gmail.com>
2026-04-27 02:08:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ connectDB()
 
 // Middleware พื้นฐาน
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json())
 
 // สร้าง HTTP server + Socket.IO
