994872e34934bfac7a09df1558950ba23632d18d
kontai <chokunzzz741852963@gmail.com>
2026-04-27 23:00:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 const io     = new Server(server, { cors: { origin: '*' } })
 
 app.use(cors({
-  origin: ['https://dataquiz-bs1u.vercel.app', 'http://127.0.0.1:5500', 'http://localhost:5500' , 'https://testdata-eta.vercel.app']
+  origin: ['https://dataquiz-nu.vercel.app', 'http://127.0.0.1:5500', 'http://localhost:5500' , 'https://testdata-eta.vercel.app']
 }))
 app.use(express.json())
 
