d8408b0f93d58152e02355362174d2550cf26e3d
kontai <chokunzzz741852963@gmail.com>
2026-04-27 08:19:00 +0700

    server.js
    server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app    = express()
 const server = http.createServer(app)
 const io     = new Server(server, { cors: { origin: '*' } })
 
-app.use(cors())
+app.use(cors({
+  origin: '*'
+}))
 app.use(express.json())
 
 app.use('/api', require('./Routes/auth'))
