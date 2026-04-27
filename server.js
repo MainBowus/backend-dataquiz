f22ecb874b2fcae23c8489e458fa4d10d232c404
kontai <chokunzzz741852963@gmail.com>
2026-04-27 23:54:51 +0700

    Fix CORS: use wildcard origin

--- server.js
+++ server.js
@@ -10,19 +10,8 @@ const app    = express()
 const server = http.createServer(app)
 const io     = new Server(server, { cors: { origin: '*' } })
 
-const ALLOWED_ORIGINS = [
-    'https://dataquiz-nu.vercel.app',
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
-    'https://testdata-eta.vercel.app'
-]
-
 app.use((req, res, next) => {
-    const origin = req.headers.origin
-    if (ALLOWED_ORIGINS.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin)
-        res.setHeader('Vary', 'Origin')
-    }
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     if (req.method === 'OPTIONS') return res.sendStatus(204)
